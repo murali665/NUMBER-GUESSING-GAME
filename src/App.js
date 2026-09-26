cba59c601fd7f558a68b7c4a409c304d644434cd
Murali665 <krishnayadav82035@gmail.com>
2026-09-26 19:48:13 +0530

    Add number guessing game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
-function App() {
+const App = () => {
+  const [guess, setGuess] = useState('');
+  const [message, setMessage] = useState('');
+  const [randomNumber] = useState(Math.floor(Math.random() * 100) + 1);
+  const [attempts, setAttempts] = useState(0);
+  const maxAttempts = 5;
+
+  const handleInputChange = (event) => {
+    setGuess(event.target.value);
+  };
+
+  const handleGuess = () => {
+    const userGuess = parseInt(guess);
+
+    if (isNaN(userGuess)) {
+      setMessage('Please enter a valid number.');
+    } else {
+      setAttempts(attempts + 1);
+
+      if (userGuess === randomNumber) {
+        setMessage(`Congratulations! You guessed the number ${randomNumber} correctly in ${attempts} attempts`);
+      } else if (userGuess < randomNumber) {
+        setMessage('Try a higher number.');
+      } else {
+        setMessage('Try a lower number.');
+      }
+
+      if (attempts >= maxAttempts) {
+        setMessage(`Game over! The number was ${randomNumber}`);
+      }
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Number Guessing Game</h1>
+      <h4>Guess a number between 1 and 100:</h4>
+      <input type="text" value={guess} onChange={handleInputChange} /><br></br>
+      <button onClick={handleGuess}>Guess</button>
+      <h3>{message}</h3>
     </div>
   );
-}
+};
 
 export default App;
